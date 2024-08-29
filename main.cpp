4d530ee09a0b33ba775134f4ebd74f83194f8764
Zayed Khan <zayedkh02@gmail.com>
2024-08-29 15:13:06 +0100

    Used for loop to help readability

--- main.cpp
+++ main.cpp
@@ -23,15 +23,16 @@ int main()
 void drawBoard(char* spaces)
 {
     std::cout << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "   " << spaces[0] << " |   " << spaces[1] << " |   " << spaces[2] << '\n';
-    std::cout << "_____|_____|_____" << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "   " << spaces[3] << " |   " << spaces[4] << " |   " << spaces[5] << '\n';
-    std::cout << "_____|_____|_____" << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "   " << spaces[6] << " |   " << spaces[7] << " |   " << spaces[8] << '\n';
-    std::cout << "     |     |     " << '\n';
+    for (int i = 0; i < 9; i += 3)
+    {
+        std::cout << "     |     |     " << '\n';
+        std::cout << "   " << spaces[i] << " |   " << spaces[i + 1] << " |   " << spaces[i + 2] << '\n';
+        if (!(i > 3))
+        {
+            std::cout << "_____|_____|_____" << '\n';
+        }
+    }
+    
     std::cout << '\n';
 }
 
